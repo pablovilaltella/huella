490c2a78aaab0778f0a198de51a6a00f9d282869
Pablo <Pablo@Popi-Inspiron>
2016-09-04 19:01:10 -0300

    ABM de persona

---
+++ Huella/src/persona/PersonaABM.java
@@ -0,0 +1,126 @@
+package persona;
+
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import db.MysqlConnect;
+
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import java.awt.Toolkit;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PersonaABM extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField textFieldApellido;
+	private JTextField textFieldNombre;
+	private MysqlConnect conection;
+	private JTextField textFieldProfesion;
+	
+	/**
+	 * Create the Dialog
+	 */
+	public PersonaABM(){
+		super("Crear Persona");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Pablo\\git\\huella\\Huella\\dist\\person_icon.png"));
+		
+		setState(Frame.NORMAL);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setResizable(false);
+		
+		setBounds(100, 100, 500, 450);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		
+		// traigo la conexion
+		conection = MysqlConnect.getDbCon();
+		
+		JLabel lblApellido = new JLabel("Apellido:");
+		lblApellido.setBounds(22, 22, 50, 20);
+		getContentPane().add(lblApellido);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(22, 53, 50, 20);
+		getContentPane().add(lblNombre);
+		
+		JLabel lblTipoDoc = new JLabel("Tipo Documento:");
+		lblTipoDoc.setBounds(22, 84, 100, 20);
+		getContentPane().add(lblTipoDoc);
+		
+		JLabel lblNumero = new JLabel("N\u00FAmero:");
+		lblNumero.setBounds(22, 115, 50, 20);
+		getContentPane().add(lblNumero);
+		
+		JComboBox cbTipoDoc = new JComboBox();
+		cbTipoDoc.setBounds(116, 84, 128, 20);
+		this.cargarCombo(cbTipoDoc);
+		getContentPane().add(cbTipoDoc);
+		
+		textFieldApellido = new JTextField();
+		textFieldApellido.setBounds(116, 22, 128, 20);
+		getContentPane().add(textFieldApellido);
+		textFieldApellido.setColumns(10);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setColumns(10);
+		textFieldNombre.setBounds(116, 53, 128, 20);
+		getContentPane().add(textFieldNombre);
+		
+		JTextField textFieldNro = new JTextField();
+		textFieldNro.setColumns(10);
+		textFieldNro.setBounds(116, 115, 128, 20);
+		getContentPane().add(textFieldNro);
+		
+		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (conection.existePersona()){
+					System.out.println("Existe la persona ");
+				}
+				else{
+					conection.guardarPersona(textFieldApellido.getText(),textFieldNombre.getText(), textFieldProfesion.getText(), cbTipoDoc.getSelectedItem().toString(), textFieldNro.getText());
+				}
+				
+			}
+		});
+		btnGuardar.setBounds(22, 197, 89, 23);
+		getContentPane().add(btnGuardar);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(143, 197, 89, 23);
+		getContentPane().add(btnCancelar);
+		
+		JLabel lblProfesion = new JLabel("Profesi\u00F3n:");
+		lblProfesion.setBounds(22, 154, 50, 20);
+		getContentPane().add(lblProfesion);
+		
+		textFieldProfesion = new JTextField();
+		textFieldProfesion.setColumns(10);
+		textFieldProfesion.setBounds(116, 154, 128, 20);
+		getContentPane().add(textFieldProfesion);
+		setVisible(true);
+	}
+
+	private void cargarCombo(JComboBox cbTipoDoc) {
+		
+		ResultSet resultado = conection.getTipoDocumentos();
+		try {
+			while (resultado.next()){
+				cbTipoDoc.addItem(resultado.getString(3));
+			}
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}
+	}
+}
