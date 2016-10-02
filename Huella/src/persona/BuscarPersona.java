ceba5d9fb20f612a7e558b0e98fdc15c2b7a0db5
Pablo <Pablo@Popi-Inspiron>
2016-10-02 20:54:32 -0300

    Agrego los datos para buscar

--- Huella/src/persona/BuscarPersona.java
+++ Huella/src/persona/BuscarPersona.java
@@ -10,6 +10,12 @@ import db.MysqlConnect;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BuscarPersona extends JFrame {
 
@@ -19,6 +25,12 @@ public class BuscarPersona extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private MysqlConnect conection;
 	private JTable table;
+	private JComboBox<String> comboBoxTD;
+	private JTextField textFieldApellido;
+	private JTextField textFieldNombre;
+	private JTextField textField;
+	private JLabel lblTipoDoc;
+	private JLabel lblNewLabel;
 
 	public BuscarPersona(){
 		
@@ -28,7 +40,7 @@ public class BuscarPersona extends JFrame {
 		setState(Frame.NORMAL);
 		setResizable(false);
 		
-		setBounds(100, 100, 600, 600);
+		setBounds(100, 100, 800, 600);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 
@@ -36,27 +48,91 @@ public class BuscarPersona extends JFrame {
 		conection = MysqlConnect.getDbCon();
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(22, 204, 553, 372);
+		scrollPane.setBounds(22, 114, 750, 446);
 		getContentPane().add(scrollPane);
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
 		
-		
 		DefaultTableModel dtm = new DefaultTableModel();
 		table.setModel(dtm);
 		
-		dtm.setColumnIdentifiers(new Object[]{"Id","Apellido","Nombre",});
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(22, 25, 46, 14);
+		getContentPane().add(lblApellido);
 		
-		ResultSet personas = conection.findPersona("pepe", "");
+		JLabel lblNombre = new JLabel("Nombre::");
+		lblNombre.setBounds(298, 25, 51, 14);
+		getContentPane().add(lblNombre);
 		
-		try {
-			while (personas.next()){
-				dtm.addRow(new Object[]{personas.getInt(1),personas.getString(2), personas.getString(3)});
+		comboBoxTD = new JComboBox<String>();
+		comboBoxTD.setBounds(125, 61, 128, 20);
+		this.cargarCombo();
+		getContentPane().add(comboBoxTD);
+		
+		textFieldApellido = new JTextField();
+		textFieldApellido.setBounds(83, 23, 170, 20);
+		getContentPane().add(textFieldApellido);
+		textFieldApellido.setColumns(10);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setColumns(10);
+		textFieldNombre.setBounds(359, 23, 170, 20);
+		getContentPane().add(textFieldNombre);
+
+		dtm.setColumnIdentifiers(new Object[]{"Id","Apellido","Nombre","Tipo doc", "Número", "Profesión"});
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO: VER COMO ACTUALIZAR LA GRILLA
+				DefaultTableModel dm = (DefaultTableModel)table.getModel();
+				dm.getDataVector().removeAllElements();
+				 	
+				ResultSet personas = conection.findPersona(textFieldApellido.getText(), textFieldNombre.getText());
+				
+				try {
+					while (personas.next()){
+						dtm.addRow(new Object[]{personas.getInt(1),personas.getString(2), personas.getString(3),personas.getString(4),personas.getString(5),personas.getString(6)});
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				dm.fireTableDataChanged();
 			}
-		} catch (SQLException e) {
+		});
+		btnBuscar.setBounds(615, 40, 89, 23);
+		getContentPane().add(btnBuscar);
+		
+		textField = new JTextField();
+		textField.setBounds(359, 61, 170, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		lblTipoDoc = new JLabel("Tipo Documento:");
+		lblTipoDoc.setBounds(22, 64, 93, 14);
+		getContentPane().add(lblTipoDoc);
+		
+		lblNewLabel = new JLabel("N\u00FAmero:");
+		lblNewLabel.setBounds(298, 64, 51, 14);
+		getContentPane().add(lblNewLabel);
+		
+		
+		setVisible(true);
+	}
+	
+	/**
+	 * Cargo los tipo de documentos en el combo
+	 */
+	private void cargarCombo() {
+		
+		ResultSet resultado = conection.getTipoDocumentos();
+		try {
+			while (resultado.next()){
+				comboBoxTD.addItem(resultado.getString(3));
+			}
+		} catch (SQLException e) {			
 			e.printStackTrace();
 		}
-		setVisible(true);
 	}
 }
